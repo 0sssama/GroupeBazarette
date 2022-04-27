1e942f108b82bd7fc9d476c370804db69e29d689
Oussama Labrahmi <olabrahm@e2r9p6.1337.ma>
2022-04-27 16:26:25 +0000

    :gear: initialized recoil

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
+import { windowState } from "atoms/states";
 import Head from "next/head";
+import { useRecoilValue } from "recoil";
 
 export default function Home() {
+  // screen width
+  const { width } = useRecoilValue(windowState);
+
   return (
     <div className="Home Page wrapper">
       <Head>
@@ -9,7 +14,8 @@ export default function Home() {
         </title>
       </Head>
       <div className="container w-full padding-x text-center">
-        <p>Welcome to Groupe Bazarette</p>
+        <p className="title primary">Welcome to Groupe Bazarette</p>
+        <p className="title secondary">current width : {width}</p>
       </div>
     </div>
   );
