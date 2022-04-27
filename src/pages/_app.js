471dfea670e33e8c027ae52460abbfd77704ea2b
Oussama Labrahmi <olabrahm@e2r9p6.1337.ma>
2022-04-27 16:07:44 +0000

    :gear: init nprogress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "styles/globals.scss";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+// Setting up NProgress : that really cool progress
+// bar at the top of the page!
+NProgress.configure({ showSpinner: true });
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
