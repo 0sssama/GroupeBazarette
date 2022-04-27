c2c0775233f629c68066aba87ea68b061d147f27
Oussama Labrahmi <olabrahm@e2r9p6.1337.ma>
2022-04-27 20:50:53 +0000

    :gear: init header nav

--- src/components/global/Header/index.js
+++ src/components/global/Header/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import Link from "next/link";
+import { BsChevronDown } from "react-icons/bs";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="Header fixed top-0 left-0 right-0 flex items-center justify-center">
+      <div className="Header-wrapper wrapper padding-x flex flex-col items-center justify-between">
+        <div className="Header-logo w-full flex items-center justify-center">
+          <Link href="/">
+            <a>
+              <p>
+                GROUPE
+                <span>BAZARETTE</span>
+              </p>
+            </a>
+          </Link>
+        </div>
+        <div className="Header-nav w-full flex items-center justify-center">
+          <div className="Header-nav-item flex items-center col-gap-s">
+            <p>LUNETTES DE SOLEIL</p>
+            <BsChevronDown />
+          </div>
+          <div className="Header-nav-item flex items-center col-gap-s">
+            <p>LUNETTES DE VUE</p>
+            <BsChevronDown />
+          </div>
+          <div className="Header-nav-item flex items-center col-gap-s">
+            <p>OFFRES</p>
+            <BsChevronDown />
+          </div>
+          <div className="Header-nav-item flex items-center col-gap-s">
+            <p>NOUVEAUTÉS</p>
+            <BsChevronDown />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
