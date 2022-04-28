3063877b2688d14c6fc46c81f4f187bcddf7822c
Oussama Labrahmi <olabrahm@e2r9p6.1337.ma>
2022-04-28 20:39:02 +0000

    :gear: finished header for mobile design

--- src/components/global/Header/index.js
+++ src/components/global/Header/index.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
-import { FiChevronDown } from "react-icons/fi";
+import { FiChevronDown, FiMenu } from "react-icons/fi";
 
 function Header() {
   return (
     <div className="Header fixed top-0 left-0 right-0 flex items-center justify-center">
-      <div className="Header-wrapper wrapper padding-x flex flex-col items-center justify-between">
-        <div className="Header-logo w-full flex items-center justify-center">
+      <div className="Header-wrapper wrapper padding-x flex items-center justify-between">
+        <div className="Header-logo flex items-center justify-center">
           <Link href="/">
             <a>
               <p>
@@ -15,7 +15,10 @@ function Header() {
             </a>
           </Link>
         </div>
-        <div className="Header-nav w-full flex items-center justify-center flex-wrap">
+        <div className="Header-ham-menu hide-for-desktop">
+          <FiMenu />
+        </div>
+        <div className="Header-nav w-full flex items-center justify-center flex-wrap hide-for-mobile">
           <div className="Header-nav-item flex items-center col-gap-s">
             <p>LUNETTES DE SOLEIL</p>
             <FiChevronDown />
