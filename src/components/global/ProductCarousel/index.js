b75f4de99fadd51f701c169bdc411faaf887890b
osama <0ssama@protonmail.com>
2022-05-07 18:32:23 +0000

    :gear: product carousel is now slidable

--- src/components/global/ProductCarousel/index.js
+++ src/components/global/ProductCarousel/index.js
@@ -1,6 +1,18 @@
+import { windowState } from "atoms/states";
 import { Section, Carousel, Product } from "components";
+import { useState } from "react";
+import { useRecoilValue } from "recoil";
 
 function ProductCarousel({ titles, products, className }) {
+  // get width of screen
+  const { width } = useRecoilValue(windowState);
+
+  // width of product image
+  const produtWidth = width > 768 ? 300 : 200;
+
+  // current product
+  const [current, setCurrent] = useState(0);
+
   return (
     <Section
       title={{
@@ -10,12 +22,27 @@ function ProductCarousel({ titles, products, className }) {
     >
       <Carousel
         active={{
-          previous: false,
-          next: true,
+          previous: current !== 0,
+          next: current !== products.length - 1,
+        }}
+        onNext={() => {
+          if (current < products.length - 1) {
+            setCurrent(current + 1);
+          }
+        }}
+        onPrevious={() => {
+          if (current > 0) {
+            setCurrent(current - 1);
+          }
         }}
         className={className}
       >
-        <div className="Products-wrapper flex items-start">
+        <div
+          className="Products-wrapper flex items-start"
+          style={{
+            transform: `translateX(${current * -produtWidth}px)`,
+          }}
+        >
           {products.map((product, index) => (
             <Product key={index} data={product} />
           ))}
