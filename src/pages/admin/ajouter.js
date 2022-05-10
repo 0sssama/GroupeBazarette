7aa919e79a9fbf58272294a873746ff4c3978292
Oussama Labrahmi <olabrahm@e2r6p14.1337.ma>
2022-05-10 13:54:38 +0100

    :gear: init product add page, init form button input components.

---
+++ src/pages/admin/ajouter.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { Logo } from "components";
+import Head from "next/head";
+
+function Ajouter() {
+  // remove header on page render
+  useEffect(() => {
+    // hiding both headers
+    const headers = document.querySelectorAll(".Header");
+    headers.forEach((header) => {
+      header.classList.add("display-none-important");
+    });
+
+    // hiding footer
+    document
+      .querySelector("footer.Footer")
+      .classList.add("display-none-important");
+  }, []);
+
+  return (
+    <div className="Ajouter Page wrapper padding-x flex flex-col items-start row-gap">
+      <Head>
+        <title>Ajouter un produit - GroupeBazarette</title>
+      </Head>
+      <Logo />
+      <h1 className="title primary">Ajouter un produit</h1>
+    </div>
+  );
+}
+
+export default Ajouter;
