e8a03da234d1a844cec8ac77124f012bfd064a43
Oussama Labrahmi <olabrahm@e1r6p10.1337.ma>
2022-05-19 12:09:59 +0100

    :gear: added search to navbar

--- src/components/global/Header/Navbar/Item.js
+++ src/components/global/Header/Navbar/Item.js
@@ -1,12 +1,19 @@
 import Link from "next/link";
 import { FiChevronDown } from "react-icons/fi";
+import { BiSearch } from "react-icons/bi";
 
 function Item({ title, url }) {
   return (
     <Link href={url}>
       <a className="no-line">
         <div className="Header-nav-item flex items-center col-gap-s">
-          <p>{title}</p>
+          {title === "SEARCH" ? (
+            <p className="search-nav">
+              <BiSearch />
+            </p>
+          ) : (
+            <p>{title}</p>
+          )}
           {/* <FiChevronDown /> */}
         </div>
       </a>
