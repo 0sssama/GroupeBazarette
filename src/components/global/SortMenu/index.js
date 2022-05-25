bf8deff8a7d1a7863a5f0fd16f988be2cbc2ca69
Oussama Labrahmi <olabrahm@e2r4p9.1337.ma>
2022-05-25 10:42:10 +0100

    :gear: finished sorting

--- src/components/global/SortMenu/index.js
+++ src/components/global/SortMenu/index.js
@@ -1,13 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "components";
 
-const categories = ["Tous", "Vasco De Gama", "Antonio Ferre"];
+const categories = ["Tous", "Vasco de Gama", "Antonio Ferre"];
 
-const genres = ["Tous", "Hommes", "Femmes"];
+const genres = ["Tous", "Homme", "Femme"];
 
 function SortMenu({ originalProducts, products, setProducts }) {
   const [category, setCategory] = useState(0);
   const [genre, setGenre] = useState(0);
+
+  useEffect(() => {
+    if (category === 0 && genre === 0) {
+      setProducts(originalProducts);
+    } else if (genre === 0) {
+      setProducts(
+        originalProducts.filter(
+          (product) => product.collectionName === categories[category]
+        )
+      );
+    } else if (category === 0) {
+      setProducts(
+        originalProducts.filter((product) => product.gender === genres[genre])
+      );
+    } else {
+      setProducts(
+        originalProducts.filter(
+          (product) =>
+            product.collectionName === categories[category] &&
+            product.gender === genres[genre]
+        )
+      );
+    }
+  }, [category, genre]);
 
   return (
     <div className="SortMenu w-full flex flex-col items-end justify-end flex-wrap col-gap row-gap-s padding-x">
@@ -18,24 +42,19 @@ function SortMenu({ originalProducts, products, setProducts }) {
             <Button
               key={key}
               type={key === category ? "primary" : "secondary"}
-              onClick={() => {
-                setCategory(key);
-                setProducts(originalProducts);
-              }}
+              onClick={() => setCategory(key)}
             >
               {cat}
             </Button>
           ))}
         </div>
+        <div className="line hide-for-mobile"></div>
         <div className="SortMenu-genres flex items-center col-gap-s">
           {genres.map((gen, key) => (
             <Button
               key={key}
               type={key === genre ? "primary" : "secondary"}
-              onClick={() => {
-                setGenre(key);
-                setProducts(originalProducts);
-              }}
+              onClick={() => setGenre(key)}
             >
               {gen}
             </Button>
